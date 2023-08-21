e2fd6974ecba2ff7a09a4273d893673827ada9a4
Flynn <flynn.ross@gmail.com>
2023-08-21 17:01:06 +1000

    fix control.launch

--- bits4bashrc-1.sh
+++ bits4bashrc-1.sh
@@ -21,10 +21,10 @@ function run349 (){
     sleep 2
     tmux new-session -s UAVTeam3\; set -g mouse on\; send-keys "roscore" C-m\; split-window -h -p 85\; select-pane -t 0\; split-window -v\;                   \
         send-keys "htop" C-m\; select-pane -t 2\; split-window -h\; select-pane -t 2\;                   \
-        send-keys "ls -l" C-m\; split-window -v -p 75\;               \
-        send-keys "sleep 5; roslaunch /${catkin_ws}/launch/control.launch" C-m\; split-window -v -p 60\;                                 \
-        send-keys "sleep 10; roslaunch qutas_lab_450 environment.launch" C-m\; split-window -v \;                                        \
-        send-keys "sleep 10; Vision code here... C-m" \;  select-pane -t 6\;                                   \
+        send-keys "sleep 5; roslaunch ${catkin_ws}/launch/control.launch" C-m\; split-window -v -p 75\;               \
+        send-keys "sleep 10; roslaunch qutas_lab_450 environment.launch" C-m\; split-window -v -p 60\;                                 \
+        send-keys "sleep 5; rosrun depthai_publisher dai_publisher" C-m\; split-window -v \;                                        \
+        send-keys "sleep 5; rosrun depthai_publisher aruco_subscriber" \;  select-pane -t 6\;                                   \
         send-keys "rosbag -a" \;  split-window -v -p 85\;                      \
         send-keys "rostopic echo /mavros/local_position/pose" C-m\; split-window -v -p 80\; select-pane -t 7\; split-window -h\; \
         send-keys "rostopic echo /mavros/vision_position/pose" C-m\; select-pane -t 9\;                                                 \
